1ea384cc3b980fe8b6b349e963e327c4622048aa
Urban.Modig <urban.modig@ericsson.com>
2017-10-17 19:18:14 +0200

    mqtt tests

--- Readme.md
+++ Readme.md
@@ -1,3 +1,28 @@
+##Elasticsearch
 ```
 sudo sysctl -w vm.max_map_count=262144
 ```
+Start by adding an index
+```
+curl -XPUT "http://localhost:9200/music/"
+```
+then add a song
+```
+curl -XPUT "http://localhost:9200/music/songs/1" -d '
+{ "name": "Deck the Halls", "year": 1885, "lyrics": "Fa la la la la" }'
+```
+look up that song
+```
+curl -XGET "http://localhost:9200/music/songs/1"
+```
+add a lyric using provided fil
+```
+curl -XPUT "http://localhost:9200/music/lyrics/2" -d @walking.json
+```
+add another lyric
+```
+curl -XPUT "http://localhost:9200/music/lyrics/1" -d @caseyjones.json
+```
+
+##MQTT
+..
---
+++ mqttelasticconnector/src/test/java/TestMQTT.java
@@ -0,0 +1,69 @@
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+import static org.apache.commons.codec.CharEncoding.UTF_8;
+
+/**
+ * Copyright (c) Ericsson AB, 2016.
+ * <p/>
+ * All Rights Reserved. Reproduction in whole or in part is prohibited
+ * without the written consent of the copyright owner.
+ * <p/>
+ * ERICSSON MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. ERICSSON SHALL NOT BE LIABLE FOR
+ * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ * <p/>
+ * User: eurbmod
+ * Date: 2017-10-17
+ */
+public class TestMQTT {
+
+    @Test
+    public void test1() throws Exception {
+        MqttClient client = new MqttClient(
+                "tcp://localhost:1883", //URI
+                MqttClient.generateClientId(), //ClientId
+                new MemoryPersistence()); //Persistence
+        client.connect();
+        client.publish(
+                "topic", // topic
+                "payload".getBytes(UTF_8), // payload
+                2, // QoS
+                false); // retained?
+    }
+
+    @Test
+    public void test() throws Exception {
+        MqttClient client = new MqttClient(
+                "tcp://localhost:1883", //URI
+                MqttClient.generateClientId(), //ClientId
+                new MemoryPersistence()); //Persistence
+
+        client.setCallback(new MqttCallback() {
+
+            @Override
+            public void connectionLost(Throwable cause) { //Called when the client lost the connection to the broker
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                System.out.println(topic + ": " + Arrays.toString(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {//Called when a outgoing publish is complete
+            }
+        });
+
+        client.connect();
+        client.subscribe("#", 1);
+        Thread.sleep(5000);
+    }
+}
