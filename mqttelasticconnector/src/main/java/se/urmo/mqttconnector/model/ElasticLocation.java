ef7d1b7410462147cf710f4090eadfb5c9239226
jiinxx <anonema@gmail.com>
2025-09-25 14:48:17 +0200

    Update ElasticLocation.java

--- mqttelasticconnector/src/main/java/se/urmo/mqttconnector/model/ElasticLocation.java
+++ mqttelasticconnector/src/main/java/se/urmo/mqttconnector/model/ElasticLocation.java
@@ -1,21 +1,6 @@
 package se.urmo.mqttconnector.model;
 
-/**
- * Copyright (c) Ericsson AB, 2016.
- * <p/>
- * All Rights Reserved. Reproduction in whole or in part is prohibited
- * without the written consent of the copyright owner.
- * <p/>
- * ERICSSON MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
- * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
- * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. ERICSSON SHALL NOT BE LIABLE FOR
- * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
- * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
- * <p/>
- * User: eurbmod
- * Date: 2017-10-20
- */
+
 public class ElasticLocation{
     private BaseLocation baseLocation = new BaseLocation();
 
